a25fe3e5641c99249908c3bd0a5b2bdbf121988d
titanium901 <nemes901@hotmail.com>
2019-09-06 16:32:18 +0300

    Refactoring
    ref network part

--- ImagesApp/Controller/ImagesViewController.swift
+++ ImagesApp/Controller/ImagesViewController.swift
@@ -14,21 +14,21 @@ class ImagesViewController: UICollectionViewController {
    
     let cache = KingfisherManager.shared.cache
     
-    var photos = [Photo]()
-    var photos2 = [Photo]()
+    let network = Network()
     var defImages = [String]()
     var responseString = String()
     var downloadTime = String()
     var connection = Bool()
     var page: Int = 2 {
         didSet {
+            print("didset page")
             DispatchQueue.global(qos: .background).async { [weak self] in
-                self?.getImages(strUrl: "https://picsum.photos/v2/list?page=" + "\(self!.page)" + "&limit=100") { () -> (Void) in
-                    print("done \(String(describing: self?.page))")
-                    
+                self?.network.getImages(strUrl: "https://picsum.photos/v2/list?page=" + "\(self!.page)" + "&limit=100") { [weak self] in
+                    print("Done \(String(describing: self?.page))")
+                    self?.collectionView.insertItems(at: [(self?.network.insertIndexPath)!])
                 }
+                
             }
-            
         }
     }
     
@@ -43,7 +43,7 @@ class ImagesViewController: UICollectionViewController {
         if CheckInternet.Connection(){
             print("Connect")
             connection = true
-            getImage(strUrl: baseUrl) { [weak self] in
+            network.getImages(strUrl: baseUrl) { [weak self] in
                 print("done")
                 DispatchQueue.main.async {
                     self?.reload()
@@ -61,16 +61,9 @@ class ImagesViewController: UICollectionViewController {
                 self?.present(ac, animated: true)
                 self?.defImages.append("back2")
             }
-            
-            
         }
-        
-        
-        
-        
-        
-       
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = true
@@ -81,9 +74,7 @@ class ImagesViewController: UICollectionViewController {
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        
-        return connection ? photos.count : defImages.count
+        return connection ? network.photos.count : defImages.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -91,7 +82,7 @@ class ImagesViewController: UICollectionViewController {
             fatalError("Unable to dequeue PersonCell")
         }
         
-        let image = photos[indexPath.item]
+        let image = network.photos[indexPath.item]
         let imageUrl: URL? = URL(string: image.download_url)
 //        cell.imageView.kf.setImage(with: imageUrl)
         
@@ -106,11 +97,7 @@ class ImagesViewController: UICollectionViewController {
     // MARK: UICollectionViewDelegate
 
     override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        if photos.count - 1 == indexPath.row {
-            
-        }
-        print(photos.count)
-        print(indexPath.row)
+
         guard page < 10 else { return }
         if indexPath.row == 50 {
             page += 1
@@ -124,125 +111,21 @@ class ImagesViewController: UICollectionViewController {
             page += 1
         } else if indexPath.row == 500 {
             page += 1
-        } else if indexPath.row == 600 {
-//            page += 1
-        } else if indexPath.row == 700 {
-//            page += 1
-        } else if indexPath.row > 800 {
-            print(page)
-//            if page >= 9 {
-//
-//            } else {
-//                page += 1
-//            }
-            
         }
-//        print(indexPath.row)
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             let index = indexPath.row
-            vc.photo = photos[index]
+            vc.photo = network.photos[index]
             vc.downloadTime = downloadTime
             navigationController?.pushViewController(vc, animated: true)
         }
     }
 
-    
-    func currentTime() -> String {
-        let formatter = DateFormatter()
-        formatter.timeStyle = .medium
-        formatter.dateStyle = .long
-        let str = formatter.string(from: Date())
-        return str.replacingOccurrences(of: "+0000", with: "", options: [], range: nil)
-        
-        
-    }
-    
-    
     func reload() {
         collectionView.reloadData()
     }
-    
-    func getImage(strUrl: String, completion: @escaping () -> (Void)) {
-        print(#function)
-        let url = URL(string: strUrl)!
-        let request = NSMutableURLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request as URLRequest) { [weak self] (data, response, error) in
-            if error != nil {
-                print(error?.localizedDescription as Any)
-                return
-            }
-            
-            do {
-                let dataJson = try JSONDecoder().decode([Photo].self, from: data!)
-                self?.photos = dataJson
-                print(dataJson)
-                
-                var index = 0
-                for _ in self!.photos {
-                    self?.photos[index].time = self?.currentTime()
-                    index += 1
-                }
-                completion()
-            } catch let error {
-                print(error.localizedDescription)
-            }
-            
-            }.resume()
-        
-    }
-    
-    func getImages(strUrl: String, completion: @escaping () -> (Void)) {
-         print(#function)
-        DispatchQueue.global(qos: .background).async {
-            let url = URL(string: strUrl)!
-            let request = NSMutableURLRequest(url: url)
-            
-            URLSession.shared.dataTask(with: request as URLRequest) { [weak self] (data, response, error) in
-                if error != nil {
-                    print(error?.localizedDescription as Any)
-                    return
-                }
-                
-                do {
-                    let dataJson = try JSONDecoder().decode([Photo].self, from: data!)
-                    self?.photos2 = dataJson
-                    print("Json Data2")
-                    if self?.photos2.isEmpty ?? true {
-                        print("returnnnnnnn")
-                         print("page \(self!.page)")
-                        return
-                    }
-                    print(dataJson)
-                    DispatchQueue.main.async {
-                        var index = 0
-                        for _ in self!.photos2 {
-                            self?.photos2[index].time = self?.currentTime()
-                            let insertIndexPath = IndexPath(item: (self?.photos.count)!, section: 0)
-                            self?.photos.append((self?.photos2[index])!)
-                            self?.collectionView.insertItems(at: [insertIndexPath])
-                            index += 1
-                        }
-                        print("page \(self!.page)")
-                    }
-                    
-                    
-                    completion()
-                } catch let error {
-                    print(error.localizedDescription)
-                }
-                
-                }.resume()
-        }
-        
-        
-        
-    }
-    
-
     
 }
 
---
+++ ImagesApp/Network.swift
@@ -0,0 +1,70 @@
+//
+//  Network.swift
+//  ImagesApp
+//
+//  Created by Yury Popov on 06/09/2019.
+//  Copyright © 2019 Yury Popov. All rights reserved.
+//
+
+import Foundation
+
+class Network {
+    
+    var photos = [Photo]()
+    var photos2 = [Photo]()
+    var insertIndexPath: IndexPath!
+    
+    func getImages(strUrl: String, completion: @escaping () -> (Void)) {
+        print("Функция")
+        print(#function)
+        DispatchQueue.global(qos: .background).async {
+            let url = URL(string: strUrl)!
+            let request = NSMutableURLRequest(url: url)
+            
+            URLSession.shared.dataTask(with: request as URLRequest) { [weak self] (data, response, error) in
+                if error != nil {
+                    print(error?.localizedDescription as Any)
+                    return
+                }
+                
+                do {
+                    let dataJson = try JSONDecoder().decode([Photo].self, from: data!)
+                    self?.photos2 = dataJson
+                    print("Json Data2")
+                    if self?.photos2.isEmpty ?? true {
+                        print("returnnnnnnn")
+                        return
+                    }
+                    print(dataJson)
+                    DispatchQueue.main.async {
+                        var index = 0
+                        for _ in self!.photos2 {
+                            self?.photos2[index].time = self?.currentTime()
+                             self?.insertIndexPath = IndexPath(item: (self?.photos.count)!, section: 0)
+                            self?.photos.append((self?.photos2[index])!)
+//                            self?.collectionView.insertItems(at: [insertIndexPath])
+                            completion()
+                            index += 1
+                        }
+                    }
+                    
+                    
+                    
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+                
+                }.resume()
+        }
+    }
+    
+    func currentTime() -> String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        formatter.dateStyle = .long
+        let str = formatter.string(from: Date())
+        return str.replacingOccurrences(of: "+0000", with: "", options: [], range: nil)
+        
+        
+    }
+}
